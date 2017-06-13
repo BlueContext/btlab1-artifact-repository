7917109dd5da179c7d8d9231c978dc6234faaccf
Kevin Baker <kevin.w.baker@live.com>
2017-06-13 17:49:21 -0500

    Create base-configuration.ps1

--- artifacts/base-configuration/base-configuration.ps1
+++ artifacts/base-configuration/base-configuration.ps1
@@ -8,7 +8,7 @@ Import-Module PackageManagement
 Import-Module PowerShellGet
 Import-Module PSDesiredStateConfiguration
 Import-Module ServerManager
-Install-PackageProvider Chocolatey -Force
+#Install-PackageProvider Chocolatey -Force
 
 Set-PackageSource PSGallery
 Install-Package xStorage -Force
@@ -16,6 +16,6 @@ Install-Package xNetworking -Force
 Install-Package xComputerManagement -Force
 Install-Package xTimeZone -Force
 
-Set-DnsClientServerAddress -InterfaceIndex 5 -ServerAddresses ("10.56.10.6","10.71.15.100")
+#Set-DnsClientServerAddress -InterfaceIndex 5 -ServerAddresses ("10.56.10.6","10.71.15.100")
 
 md c:\DSC
