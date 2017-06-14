c84b19cf46cac6861e9de6fe5520ef3c68a1b8a8
Kevin Baker <kevin.w.baker@live.com>
2017-06-14 11:40:17 -0500

    Create base-setup.ps1

--- artifacts/nano-base-configuration/base-setup.ps1
+++ artifacts/nano-base-configuration/base-setup.ps1
@@ -2,19 +2,13 @@
 
 Param(
     [Parameter(Mandatory=$true)]$dnsServer1,
-    [Parameter(Mandatory=$false)]$dnsServer2
+    [Parameter(Mandatory=$true)]$dnsServer2
 )
 
-if($dnsServer2 = $null){
-     Set-DnsClientServerAddress -InterfaceIndex 2 -ServerAddresses ("$dnsServer1")
- }
- else
- {
-     Set-DnsClientServerAddress -InterfaceIndex 2 -ServerAddresses ("$dnsServer1","$dnsServer2")
- }
+Set-DnsClientServerAddress -InterfaceIndex 2 -ServerAddresses ("$dnsServer1","$dnsServer2")
 
 Set-NetFirewallProfile -Name Domain -Enabled False
-#Set-NetFirewallProfile -Name Public -Enabled False
+Set-NetFirewallProfile -Name Public -Enabled False
 Set-NetFirewallProfile -Name Private -Enabled False
 
 Save-Module -Path "$env:ProgramFiles\WindowsPowerShell\Modules" -Name NanoServerPackage -MinimumVersion 1.0.0.0
